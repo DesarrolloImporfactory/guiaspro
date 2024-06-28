f650478f810cbb60912bf63d3bb3a6d370937391
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-28 11:59:16 -0500

    first commit

---
+++ Config/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define("HOST", $_ENV['HOST']);
+define("USER", $_ENV['USER']);
+define("PASSWORD", $_ENV['PASSWORD']);
+define("DB", $_ENV['DB']);
+define("CHARSET", $_ENV['CHARSET']);
