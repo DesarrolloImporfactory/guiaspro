cc085cb76c79e735fe796da861d6b66be7f78252
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-29 11:17:25 -0500

    Sistema Speed

--- Models/SpeedModel.php
+++ Models/SpeedModel.php
@@ -21,7 +21,7 @@ class SpeedModel extends Query
     {
         $sql = "SELECT MAX(id_speed) AS id FROM guias_speed;";
         $data = $this->select($sql);
-
+        print_r($data);
         if (empty($data[0])) {
             $guia = "SPD0000001";
         } else {
