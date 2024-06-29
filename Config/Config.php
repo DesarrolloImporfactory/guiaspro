0514abf3bd279b9b0ec83e9cae03dbd8b51dc5f3
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-29 10:51:04 -0500

    Sistema Speed

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable("./");
 $dotenv->load();
 
 define("HOST", $_ENV['HOST']);
