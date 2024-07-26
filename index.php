ee3b0716d4d70ed2aa594681c2a1475075b4465e
Jeimy Jara <jeimyjara@hotmail.com>
2024-07-26 10:18:35 -0500

    XD

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: * ");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require_once 'Config/Config.php';
 $rute = !empty($_GET['url']) ? $_GET['url'] : 'Home/index';
 $array = explode('/', $rute);
