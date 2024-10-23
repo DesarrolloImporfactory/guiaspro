d102338685c61fa8ddc7b799609efabe11194393
Jeimy Jara <jeimyjara@hotmail.com>
2024-10-23 16:24:51 -0500

    XD

--- Models/GintracomModel.php
+++ Models/GintracomModel.php
@@ -97,10 +97,10 @@ class GintracomModel extends Query
                         $data3 = mysqli_query($this->market, $sql);
                         $data3 = mysqli_fetch_all($data3, MYSQLI_ASSOC);
                         if (count($data3) > 0) {
-                            $sql = "UPDATE novedades SET estado_novedad = '" . $dato["estado"] . "', novedad = '" . $dato["novedades"]["nombreNovedad"] . "', fecha = '" . $dato["novedades"]["fechaNovedad"] . "' WHERE guia_novedad = '" . $guia . "'";
+                            $sql = "UPDATE novedades SET estado_novedad = '" . $dato["novedades"]["codigoNovedad"] . "', novedad = '" . $dato["novedades"]["nombreNovedad"] . "', fecha = '" . $dato["novedades"]["fechaNovedad"] . "' WHERE guia_novedad = '" . $guia . "'";
                             $response = mysqli_query($this->market, $sql);
                         } else {
-                            $sql = "INSERT INTO novedades (guia_novedad, cliente_novedad, estado_novedad, novedad, tracking, fecha, id_plataforma) VALUES ( '" . $guia . "', '" . $nombreD . "', '" . $dato["estado"] . "', '" . $dato["novedades"]["nombreNovedad"] . "', 'https://ec.gintracom.site/web/site/tracking', '" . $dato["novedades"]["fechaNovedad"] . "', '" . $plataforma . "')";
+                            $sql = "INSERT INTO novedades (guia_novedad, cliente_novedad, estado_novedad, novedad, tracking, fecha, id_plataforma) VALUES ( '" . $guia . "', '" . $nombreD . "', '" . $dato["novedades"]["codigoNovedad"] . "', '" . $dato["novedades"]["nombreNovedad"] . "', 'https://ec.gintracom.site/web/site/tracking', '" . $dato["novedades"]["fechaNovedad"] . "', '" . $plataforma . "')";
                             $response = mysqli_query($this->market, $sql);
                         }
                     }
