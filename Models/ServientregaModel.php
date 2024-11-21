273f4a06403ffb671550e2b5946cb7f1563ec536
Jeimy Jara <jeimyjara@hotmail.com>
2024-11-21 09:25:26 -0500

    Servientrega

--- Models/ServientregaModel.php
+++ Models/ServientregaModel.php
@@ -489,7 +489,7 @@ class ServientregaModel extends Query
 
     public function validarGuias()
     {
-        $sql = "SELECT guia, estado_guia FROM `cabecera_cuenta_pagar` where guia REGEXP '^[0-9]+$' and estado_guia >=300 and estado_guia < 400 and visto= 0;";
+        $sql = "SELECT guia, estado_guia FROM `cabecera_cuenta_pagar` where guia REGEXP '^[0-9]+$' and estado_guia >=100 and estado_guia < 400 and visto= 0;";
         $result = mysqli_query($this->market, $sql);
         $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
